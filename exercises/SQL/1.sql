35c6f5e43c6cda1b669cc9fa8e8c904cb31336ac
Andreas Svensson <andreas.svensson@iths.se>
2023-04-25 15:23:16 +0200

    exercise 1A done

---
+++ exercises/SQL/1.sql
@@ -0,0 +1,33 @@
+-- ###################
+-- ### Exercise 1A ###
+-- ###################
+
+SELECT
+	Name,
+	Red,
+	Green,
+	Blue
+INTO
+	Colors2
+FROM
+	Colors
+
+SELECT
+	Name,
+	'#' +
+	CONVERT(VARCHAR(2), CONVERT(VARBINARY(1), Red), 2) +
+	CONVERT(VARCHAR(2), CONVERT(VARBINARY(1), Green), 2) +
+	CONVERT(VARCHAR(2), CONVERT(VARBINARY(1), Blue), 2)
+	AS 'Code',
+	Red,
+	Green,
+	Blue
+FROM
+	Colors2
+
+-->
+
+-- Name            Code        Red     Green    Blue
+-- aliceblue       #F0F8FF     240     248      255
+-- antiquewhite    #FAEBD7     250     235      215
+-- aqua            #00FFFF     0       255      255
